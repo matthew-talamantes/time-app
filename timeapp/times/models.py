cd9453cb4e7cc0c784a84991a2e5c2b23bc114bc
Constantus <38108928+Constantus@users.noreply.github.com>
2022-11-09 19:45:43 -0500

    Start building Client model

--- timeapp/times/models.py
+++ timeapp/times/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+import uuid
+
+from useraccount.models import CustomUser
+
 # Create your models here.
+
+class Client(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(CustomUser, editable=False, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, default='', blank=False)
+    company = models.CharField(max_length=50, default='', blank=True)
+    
